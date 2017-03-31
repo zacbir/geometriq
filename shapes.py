6e3a1999e1d6bcf9103e1de1c83846469add253b
Zachery Bir <zbir@apple.com>
2017-03-31 16:15:54 -0400

    Getting ready for partial circles, porting over some old code

--- shapes.py
+++ shapes.py
@@ -84,6 +84,89 @@ class Shape(object):
         canvas.polygon(self.points)
 
 
+class QuarterCircle(Shape):
+
+    def __init__(self, center, size):
+        super(QuarterCircle, self).__init__(center, size)
+
+        self.offset = (4.0 * size * (sqrt(2) - 1)) / 3.0
+
+
+class NorthwestQuarterCircle(QuarterCircle):
+    pass
+
+
+class NortheastQuarterCircle(QuarterCircle):
+    pass
+
+
+class SoutheastQuarterCircle(QuarterCircle):
+    pass
+
+
+class SouthwestQuarterCircle(QuarterCircle):
+    pass
+
+
+class HalfCircle(QuarterCircle):
+    """
+
+    for i in xrange(blits):
+        for j in xrange(blits - 1):
+            if i % 2 == 0:
+                p1 = Point(x + ((i + 1) * step), y + (j * step))
+                p2 = Point(x + (i * step), y + (j * step) + step)
+                p3 = Point(x + ((i + 1) * step), y + (j * step) + 2 * step)
+                pointsmap = {p1 : {'point': p2,
+                                   'cp1': Point(p1.x - offset, p1.y),
+                                   'cp2': Point(p2.x, p2.y - offset)},
+                             p2 : {'point': p3,
+                                   'cp1': Point(p2.x, p2.y + offset),
+                                   'cp2': Point(p3.x - offset, p3.y)}}
+            else:
+                p1 = Point(x + (i * step), y + (j * step))
+                p2 = Point(x + (i * step) + step, y + (j * step) + step)
+                p3 = Point(x + (i * step), y + (j * step) + 2 * step)
+                pointsmap = {p1 : {'point': p2,
+                                   'cp1': Point(p1.x + offset, p1.y),
+                                   'cp2': Point(p2.x, p2.y - offset)},
+                             p2 : {'point': p3,
+                                   'cp1': Point(p2.x, p2.y + offset),
+                                   'cp2': Point(p3.x + offset, p3.y)}}
+
+            path = CGPathCreateMutable()
+            CGContextSetRGBFillColor(ctx, 0.95, 0.85, 0.55, random.random() * 1.0)
+            CGPathMoveToPoint(path, None, p1.x, p1.y)
+            cp1 = pointsmap[p1]['cp1']
+            cp2 = pointsmap[p1]['cp2']
+            CGPathAddCurveToPoint(path, None, cp1.x, cp1.y, cp2.x, cp2.y, p2.x, p2.y)
+            cp1 = pointsmap[p2]['cp1']
+            cp2 = pointsmap[p2]['cp2']
+            CGPathAddCurveToPoint(path, None, cp1.x, cp1.y, cp2.x, cp2.y, p3.x, p3.y)
+            CGPathAddLineToPoint(path, None, p1.x, p1.y)
+            CGContextAddPath(ctx, path)
+            CGContextDrawPath(ctx, kCGPathFill)
+
+    """
+    pass
+
+
+class NorthHalfCircle(HalfCircle):
+    pass
+
+
+class EastHalfCircle(HalfCircle):
+    pass
+
+
+class SouthHalfCircle(HalfCircle):
+    pass
+
+
+class WestHalfCircle(HalfCircle):
+    pass
+
+
 class Circle(Shape):
 
     def __init__(self, center, size):
