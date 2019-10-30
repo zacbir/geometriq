96c3442d5f7309cc233f57d5aaaa5149aeaa2ea4
Zachery Bir <zbir@zacbir.net>
2019-10-30 13:53:23 -0400

    Add seed to the CLI so we can recreate images later

--- geometriq/scripts/__init__.py
+++ geometriq/scripts/__init__.py
@@ -1,7 +1,10 @@
 from datetime import datetime
+from math import floor
 import os
 import os.path
+import random
 import sys
+from time import time
 
 import click
 
@@ -15,7 +18,8 @@ from geometriq import *
 @click.option('--geometriq-directory', help='directory where geometriq scripts can be found', type=click.Path(exists=True, file_okay=False, dir_okay=True), envvar='GEOMETRIQ_DIRECTORY')
 @click.option('--output-dir', help='directory where resulting images should be saved', type=click.Path(exists=True, file_okay=False, dir_okay=True), envvar='GEOMETRIQ_OUTPUT_DIRECTORY')
 @click.option('--contrast', '-c', help='contrast theme', type=click.Choice(['dark', 'light', 'black', 'white', 'clear']), default='light')
-def geometriq_cli(geometriq_script, width, height, geometriq_directory, output_dir, contrast):
+@click.option('--seed', '-s', help='randomization seed', type=int)
+def geometriq_cli(geometriq_script, width, height, geometriq_directory, output_dir, contrast, seed):
 
     background_fills = {
         'dark': base03,
@@ -33,12 +37,17 @@ def geometriq_cli(geometriq_script, width, height, geometriq_directory, output_d
         'clear': base01
     }
 
+    if not seed:
+        seed = floor(time())
+
+    random.seed(seed)
+        
     DEBUG = os.getenv('GEOMETRIQ_DEBUG', False)
 
     sys.path.append(geometriq_directory)
 
     script_name = os.path.splitext(os.path.basename(geometriq_script))[0]
-    dated_name = "{}_{}x{}_{}".format(script_name, width, height, datetime.now().strftime('%Y-%m-%d-%H:%M:%S'))
+    dated_name = f"{script_name}_{width}x{height}_{seed}"
 
     script = __import__('{}'.format(script_name), globals(), locals(), ['draw'], 0)
 
